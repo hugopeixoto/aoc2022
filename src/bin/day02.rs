4f7561be018321f606e2a2e6780b6fdd262e4594
Hugo Peixoto <hugo.peixoto@gmail.com>
2022-12-03 04:36:42 +0000

    Optimize day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,41 +1,24 @@
 #![feature(test)]
 extern crate test;
 
-fn score1(line: &str) -> usize {
-    match line {
-        "A X" => 1 + 3,
-        "A Y" => 2 + 6,
-        "A Z" => 3 + 0,
-        "B X" => 1 + 0,
-        "B Y" => 2 + 3,
-        "B Z" => 3 + 6,
-        "C X" => 1 + 6,
-        "C Y" => 2 + 0,
-        "C Z" => 3 + 3,
-        _ => { panic!("unknown matchup: {}", line); }
-    }
+fn b_score(a: usize, b: usize) -> usize {
+  [3, 6, 0][(b+3-a)%3]
 }
 
-fn score2(line: &str) -> usize {
-    match line {
-        "A X" => 3 + 0,
-        "A Y" => 1 + 3,
-        "A Z" => 2 + 6,
-        "B X" => 1 + 0,
-        "B Y" => 2 + 3,
-        "B Z" => 3 + 6,
-        "C X" => 2 + 0,
-        "C Y" => 3 + 3,
-        "C Z" => 1 + 6,
-        _ => { panic!("unknown matchup: {}", line); }
-    }
+fn other(a: usize, score: usize) -> usize {
+    (a + score/3 + 3 - 2) % 3 + 1
 }
 
 pub fn day02(input: String) -> (usize, usize) {
-    let p1 = input.lines().map(score1).sum();
-    let p2 = input.lines().map(score2).sum();
-
-    (p1, p2)
+    input
+        .lines()
+        .map(|line| (
+            match &line[0..1] { "A" => 1, "B" => 2, "C" => 3, _ => { panic!(); } },
+            match &line[2..] { "X" => 1, "Y" => 2, "Z" => 3, _ => { panic!(); } }
+        ))
+        .map(|(a,b)| (b_score(a,b) + b, (b-1)*3 + other(a, (b-1)*3)))
+        .reduce(|(a,b), (c,d)| (a+c, b+d))
+        .unwrap()
 }
 
 aoc2022::day!(day02, bench_day02);
